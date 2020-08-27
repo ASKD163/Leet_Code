0bfeca1c7c4453f3d4471828ceb2a599273a9323
SHIKI <32726941+9SHIKI9@users.noreply.github.com>
2020-08-27 17:12:44 +0800

    Create 66.py

---
+++ Python/66.py
@@ -0,0 +1,15 @@
+#加一
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i = 1
+        while i:
+            digits[-i] += 1
+            if digits[-i] == 10:
+                digits[-i] = 0
+                if i == len(digits):
+                    digits.insert(0,0)
+                i += 1
+            else:
+                i = 0
+
+        return digits
