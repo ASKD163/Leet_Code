5f209f0deb1e8fa2263f91db12ec4060fccf4b24
SHIKI <32726941+9SHIKI9@users.noreply.github.com>
2020-09-10 12:38:17 +0800

    Update 39.py

--- Python/39.py
+++ Python/39.py
@@ -15,3 +15,21 @@ class Solution:
         DFS([],0)
         
         return res 
+
+
+##剪枝
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def DFS(tmp,sum_t,i):
+            if sum_t == target:
+                res.append(tmp)
+                return
+
+            for i in range(i,len(candidates)):
+                if sum_t + candidates[i] <= target:
+                    DFS(tmp + [candidates[i]],sum_t + candidates[i],i)
+
+        res = []
+        DFS([],0,0)
+        
+        return res
